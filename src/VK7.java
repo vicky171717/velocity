3bd161819167b36b1ed2eea7a339d4ba9bef39c3
surmabhopaly <thartharirocks@gmail.com>
2022-09-18 17:33:21 +0530

    my first commit program

---
+++ src/VK7.java
@@ -0,0 +1,16 @@
+
+public class VK7 {
+	 int ans=1;
+	 String ans_from="VICKY";
+	 String ans_to= "SALMAN";
+	  void dp()
+	  {
+		  System.out.println("plz keep patience Mem little bit busy");
+	  }
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/VK8.java
@@ -0,0 +1,29 @@
+
+public class VK8 extends VK7
+{
+	int ans=2;
+	String ans_to = "SALMAN";
+	
+ void cp()
+ {
+System.out.println("ANSWER : "+ super.ans);
+System.out.println("ans_to : "+ super.ans_to);
+ super.dp();
+ System.out.println("anw_ from : "+ super.ans_from);
+ System.out.println("---->------>------>----");
+		 
+System.out.println("ans :" + this.ans);
+System.out.println("ans_to : "+this.ans_to);
+System.out.println("MEM will take lecture on sunday if she get time");
+ System.out.println("ans_from :" + super.ans_from);
+	 }
+
+public static void main(String[] args) {
+	VK8 obj = new VK8();
+	obj.cp();
+	
+		
+
+	}
+
+}
