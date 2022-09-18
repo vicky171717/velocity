4b713fe7e75fcfa9cfb6b53d4f731e7edf14feec
surmabhopaly <thartharirocks@gmail.com>
2022-09-18 20:21:50 +0530

    delete my 2nd code in main branch

--- src/Newclass.java
+++ src/Newclass.java
@@ -3,7 +3,7 @@ public class Newclass {
 
 	public static void main(String[] args) {
 		System.out.println("this is my new class added");
-		
+		System.out.println("this is my 2nd class  in main");
 	}
 
 }
